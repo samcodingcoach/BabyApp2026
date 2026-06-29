ddd6a7a9aeb073c38af7091d3bc05c78f4db1b9c
samsu007 <itdev.nuansa@gmail.com>
2026-06-29 09:22:07 +0800

    Fix

--- admin/bayi/bayi.php
+++ admin/bayi/bayi.php
@@ -101,96 +101,120 @@ include '../includes/sidebar.php';
                     <!-- ID Member dikunci mati ke parent saat ini -->
                     <input type="hidden" name="id_member" id="id_member" value="<?= htmlspecialchars($id_member) ?>">
                     
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Nama Anak/Bayi *</label>
-                                <div class="col-sm-8">
-                                    <input type="text" class="form-control" name="nama_bayi" id="nama_bayi" required>
+                    <ul class="nav nav-tabs nav-tabs-custom" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active font-weight-bold" data-toggle="tab" href="#tab-identitas-anak" role="tab">Identitas Anak</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link font-weight-bold" data-toggle="tab" href="#tab-detail-anak" role="tab">Detail Anak</a>
+                        </li>
+                    </ul>
+
+                    <div class="tab-content p-3 text-muted">
+                        <!-- TAB 1: IDENTITAS ANAK -->
+                        <div class="tab-pane active" id="tab-identitas-anak" role="tabpanel">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Nama Anak/Bayi *</label>
+                                        <div class="col-sm-12">
+                                            <input type="text" class="form-control" name="nama_bayi" id="nama_bayi" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Tanggal Lahir</label>
+                                        <div class="col-sm-12">
+                                            <input type="date" class="form-control" name="tanggal_lahir" id="tanggal_lahir">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Anak Ke-</label>
+                                        <div class="col-sm-12">
+                                            <input type="number" class="form-control" name="anak_ke" id="anak_ke" min="1" placeholder="Cth: 1">
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Anak Ke-</label>
-                                <div class="col-sm-8">
-                                    <input type="number" class="form-control" name="anak_ke" id="anak_ke" min="1" placeholder="Cth: 1">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Tanggal Lahir</label>
-                                <div class="col-sm-8">
-                                    <input type="date" class="form-control" name="tanggal_lahir" id="tanggal_lahir">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Jenis Kelamin</label>
-                                <div class="col-sm-8">
-                                    <select class="form-control select2" name="jenis_kelamin" id="jenis_kelamin" style="width: 100%;">
-                                        <option value="">-- Belum Diset --</option>
-                                        <option value="1">Laki-Laki</option>
-                                        <option value="0">Perempuan</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Berat Badan (Kg)</label>
-                                <div class="col-sm-8">
-                                    <input type="number" step="0.01" class="form-control" name="berat_kg" id="berat_kg" placeholder="Cth: 4.5">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Tinggi/Panjang (Cm)</label>
-                                <div class="col-sm-8">
-                                    <input type="number" step="0.1" class="form-control" name="tinggi_cm" id="tinggi_cm" placeholder="Cth: 50.5">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Jenis Kelamin</label>
+                                        <div class="col-sm-12">
+                                            <select class="form-control select2" name="jenis_kelamin" id="jenis_kelamin" style="width: 100%;">
+                                                <option value="">-- Belum Diset --</option>
+                                                <option value="1">Laki-Laki</option>
+                                                <option value="0">Perempuan</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Foto Bayi (JPG/PNG)</label>
+                                        <div class="col-sm-12">
+                                            <input type="file" class="dropify" name="photo" accept="image/jpeg, image/png, image/webp" data-height="100">
+                                            <small class="form-text text-muted">File lama otomatis akan tertimpa.</small>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Lingkar Kepala (Cm)</label>
-                                <div class="col-sm-8">
-                                    <input type="number" step="0.1" class="form-control" name="lingkar_kepala_cm" id="lingkar_kepala_cm">
+                        
+                        <!-- TAB 2: DETAIL ANAK -->
+                        <div class="tab-pane" id="tab-detail-anak" role="tabpanel">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Berat Badan (Kg)</label>
+                                        <div class="col-sm-12">
+                                            <input type="number" step="0.01" class="form-control" name="berat_kg" id="berat_kg" placeholder="Cth: 4.5">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Tinggi/Panjang (Cm)</label>
+                                        <div class="col-sm-12">
+                                            <input type="number" step="0.1" class="form-control" name="tinggi_cm" id="tinggi_cm" placeholder="Cth: 50.5">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Lingkar Kepala (Cm)</label>
+                                        <div class="col-sm-12">
+                                            <input type="number" step="0.1" class="form-control" name="lingkar_kepala_cm" id="lingkar_kepala_cm">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Golongan Darah</label>
+                                        <div class="col-sm-12">
+                                            <select class="form-control font-weight-bold text-danger select2" name="golongan_darah" id="golongan_darah" style="width: 100%;">
+                                                <option value="">-- Belum Diset --</option>
+                                                <option value="A">A</option>
+                                                <option value="B">B</option>
+                                                <option value="AB">AB</option>
+                                                <option value="O">O</option>
+                                            </select>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Golongan Darah</label>
-                                <div class="col-sm-8">
-                                    <select class="form-control font-weight-bold text-danger select2" name="golongan_darah" id="golongan_darah" style="width: 100%;">
-                                        <option value="">-- Belum Diset --</option>
-                                        <option value="A">A</option>
-                                        <option value="B">B</option>
-                                        <option value="AB">AB</option>
-                                        <option value="O">O</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Riwayat Alergi Khusus</label>
-                                <div class="col-sm-8">
-                                    <input type="hidden" name="alergi" id="alergi_hidden">
-                                    <div id="alergi_editor" style="height: 80px;"></div>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Catatan Medis</label>
-                                <div class="col-sm-8">
-                                    <input type="hidden" name="keterangan" id="keterangan_hidden">
-                                    <div id="keterangan_editor" style="height: 80px;"></div>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Status Profil Anak</label>
-                                <div class="col-sm-8">
-                                    <select class="form-control font-weight-bold select2" name="is_active" id="is_active" style="width: 100%;">
-                                        <option value="1">Aktif</option>
-                                        <option value="0" class="text-danger">Sembunyikan / Non-Aktif</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Foto Bayi (JPG/PNG)</label>
-                                <div class="col-sm-8">
-                                    <input type="file" class="dropify" name="photo" accept="image/jpeg, image/png, image/webp">
-                                    <small class="form-text text-muted">File lama otomatis akan tertimpa.</small>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Riwayat Alergi Khusus</label>
+                                        <div class="col-sm-12">
+                                            <input type="hidden" name="alergi" id="alergi_hidden">
+                                            <div id="alergi_editor" style="height: 80px;"></div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Catatan Medis</label>
+                                        <div class="col-sm-12">
+                                            <input type="hidden" name="keterangan" id="keterangan_hidden">
+                                            <div id="keterangan_editor" style="height: 80px;"></div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Status Profil Anak</label>
+                                        <div class="col-sm-12">
+                                            <select class="form-control font-weight-bold select2" name="is_active" id="is_active" style="width: 100%;">
+                                                <option value="1">Aktif</option>
+                                                <option value="0" class="text-danger">Sembunyikan / Non-Aktif</option>
+                                            </select>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -347,6 +371,9 @@ function showForm() {
     document.getElementById('id_member').value = ID_MEMBER;
     document.getElementById('formTitle').innerText = 'Daftarkan Rekam Medis Anak Baru';
     
+    // Reset tab to default
+    $('.nav-tabs a[href="#tab-identitas-anak"]').tab('show');
+    
     if($().select2) {
         $('#jenis_kelamin').val('').trigger('change');
         $('#golongan_darah').val('').trigger('change');
@@ -366,6 +393,10 @@ function hideForm() {
 
 function editData(index) {
     document.getElementById('formTitle').innerText = 'Edit Rekam Medis Anak';
+    
+    // Reset tab to default
+    $('.nav-tabs a[href="#tab-identitas-anak"]').tab('show');
+    
     const item = currentList[index];
     
     document.getElementById('id_bayi').value = item.id_bayi;
